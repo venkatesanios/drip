1f34f78c36a5a95378857e6ceeffc1da64fc9b15
Kamaraj Niagara <niagarakamaraj@gmail.com>
2025-12-25 17:06:48 +0530

    code updated

--- lib/views/customer/widgets/connection_banner.dart
+++ lib/views/customer/widgets/connection_banner.dart
@@ -1,5 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../StateManagement/mqtt_payload_provider.dart';
+import '../../../models/customer/site_model.dart';
 import '../../../view_models/customer/customer_screen_controller_view_model.dart';
 
 class ConnectionBanner extends StatelessWidget {
@@ -7,6 +12,7 @@ class ConnectionBanner extends StatelessWidget {
   final int commMode;
 
   const ConnectionBanner({super.key, required this.vm, required this.commMode});
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,8 +28,15 @@ class ConnectionBanner extends StatelessWidget {
         ),
       );
     }else if (vm.isNotCommunicate) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _updatePayloadOnOffline(context);
+      });
       return _buildBanner('NO COMMUNICATION TO CONTROLLER', Colors.red.shade300);
     }else if (vm.powerSupply == 0) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _updatePayloadOnOffline(context);
+      });
+
       return _buildBanner('NO POWER SUPPLY TO CONTROLLER', Colors.red.shade300);
     }
     return const SizedBox();
@@ -35,6 +48,66 @@ class ConnectionBanner extends StatelessWidget {
       color: color,
       alignment: Alignment.center,
       child: Text(text, style: const TextStyle(fontSize: 13, color: Colors.white)),
+    );
+  }
+
+  void _updatePayloadOnOffline(BuildContext context) {
+    final master = vm.mySiteList.data[vm.sIndex].master[vm.mIndex];
+    final LiveMessage? live = master.live;
+
+    final LiveMessage? updatedLive = live?.update2402OnNoComm();
+
+    Provider.of<MqttPayloadProvider>(context, listen: false)
+        .updateReceivedPayload(
+      jsonEncode(updatedLive?.toJson() ?? _defaultPayload()),
+      true,
+    );
+  }
+
+  String _defaultPayload() => '''
+  {
+    "cC": "00000000",
+    "cM": {
+      "WifiStrength": 0, "PowerSupply": 0
+    },
+    "cD": "0000-00-00",
+    "cT": "00:00:00",
+    "mC": "2400"
+  }''';
+
+}
+
+extension LiveMessageOfflineUpdate on LiveMessage {
+  LiveMessage update2402OnNoComm() {
+    if (cM is! Map<String, dynamic>) return this;
+
+    final updatedCM = Map<String, dynamic>.from(cM);
+
+    if (!updatedCM.containsKey('2402')) return this;
+
+    final raw = updatedCM['2402'];
+    if (raw is! String || raw.isEmpty) return this;
+
+    final updated2402 = raw
+        .split(';')
+        .map((entry) {
+      final parts = entry.split(',');
+      if (parts.length >= 2) {
+        // force OFF
+        parts[1] = '0';
+      }
+      return parts.join(',');
+    })
+        .join(';');
+
+    updatedCM['2402'] = updated2402;
+
+    return LiveMessage(
+      cC: cC,
+      cM: updatedCM,
+      cD: cD,
+      cT: cT,
+      mC: mC,
     );
   }
 }
