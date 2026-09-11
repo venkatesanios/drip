bc23ed54f301d443def56ca4c1feee6bbbf7ec78
Siva Prakash <sivaprakash.niagara@gmail.com>
2026-09-11 17:29:23 +0530

    source configuration tank commented

--- lib/modules/config_maker/view/source_configuration.dart
+++ lib/modules/config_maker/view/source_configuration.dart
@@ -138,7 +138,9 @@ class _SourceConfigurationState extends State<SourceConfiguration> {
                                 trailing: IntrinsicWidth(
                                   child: CustomDropDownButton(
                                       value: getTankCodeToString(source.sourceType),
-                                      list: const ['Tank', 'Sump', 'Well', 'Bore', 'Fertilizer Source', 'Fertilizer Tank', 'Others'],
+                                      list: const ['Tank', 'Sump', 'Well', 'Bore',
+                                        // 'Fertilizer Source', 'Fertilizer Tank',
+                                        'Others'],
                                       onChanged: (value){
                                         setState(() {
                                           source.sourceType = getTankStringToCode(value!);
